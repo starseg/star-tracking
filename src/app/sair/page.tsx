5c623cab010f9ff66e3f4ee33eb6e9f1ce8f0b84
Micael Miranda <micaelmiranda124@gmail.com>
2024-03-13 11:06:47 -0300

    correção 4 - logout

---
+++ src/app/sair/page.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+export default function Sair() {
+  return (
+    <>
+      <div className="flex flex-col gap-4 w-full h-screen items-center justify-center">
+        <Image
+          src="/logo.svg"
+          alt="Logo Star Tracking"
+          width={352}
+          height={46}
+          priority={true}
+          className="max-w-[80%] hidden md:block"
+        />
+        <h1 className="text-xl">Sua conta foi desconectada.</h1>
+        <Link href="/api/logout" className="hidden">
+          OK
+        </Link>
+        <Link href="/login">
+          <Button>Voltar para o login</Button>
+        </Link>
+      </div>
+    </>
+  );
+}
